c5448ce40cb6a5258c3da50278078ddbe1c7fb10
wiedenfeld <andreas.wiedenfeld@3m5.de>
2015-02-20 13:25:25 +0100

    check in Annotation Source files

---
+++ src/main/java/de/_3m5/restgen/api/JSONResponseArrayType.java
@@ -0,0 +1,21 @@
+/**
+ * (C) 2014 by 3m5. Media GmbH. http://www.3m5.de
+ */
+package de._3m5.restgen.api;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation for marking a response array object of a REST endpoint method.
+ * 
+ * @author osterrath
+ * 
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface JSONResponseArrayType {
+	Class<?>[] value();
+}
